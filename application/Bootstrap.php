c7cc854fa6d94f3b167844022e7e1ed9ffd66a6e
watson <lihuasheng@wapwei.com>
2016-09-24 10:30:59 +0800

    全局函数库加载

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -9,23 +9,31 @@
  */
 class Bootstrap extends Yaf\Bootstrap_Abstract{
 
-    public function _initConfig() {
+    public function _initConfig()
+	{
 		//把配置保存起来
 		$arrConfig = Yaf\Application::app()->getConfig();
 		Yaf\Registry::set('config', $arrConfig);
 	}
 
-	public function _initPlugin(Yaf\Dispatcher $dispatcher) {
+	public function _initCommonFunction()
+	{
+		Yaf\Loader::import(Yaf\Registry::get('config')->application->directory . '/Common/Functions.php');
+	}
+	public function _initPlugin(Yaf\Dispatcher $dispatcher)
+	{
 		//注册一个插件
 		$objSamplePlugin = new SamplePlugin();
 		$dispatcher->registerPlugin($objSamplePlugin);
 	}
 
-	public function _initRoute(Yaf\Dispatcher $dispatcher) {
+	public function _initRoute(Yaf\Dispatcher $dispatcher)
+	{
 		//在这里注册自己的路由协议,默认使用简单路由
 	}
 
-	public function _initView(Yaf\Dispatcher $dispatcher){
+	public function _initView(Yaf\Dispatcher $dispatcher)
+	{
 		//在这里注册自己的view控制器，例如smarty,firekylin
 	}
 }
