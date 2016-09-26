e093e61b3a57b857ae25cafc5dfdd47acaec1efd
34team <34team@wapwei.com>
2016-09-26 14:56:01 +0800

    数据库配置调试

--- application/library/think/Config.php
+++ application/library/think/Config.php
@@ -8,7 +8,6 @@ namespace think;
 class Config
 {
     // 参数作用域
-    private static $range = '_sys_';
     /**
      * 获取配置参数 为空则获取所有配置
      * @param string    $name 配置参数名（支持二级配置 .号分割）
@@ -20,6 +19,6 @@ class Config
         if(!$name){
             return null;
         }
-        return Yaf\Registry(get('config')->$name);
+        return \Yaf\Registry::get('config')->$name;
     }
 }
