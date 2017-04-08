1a15e5b17304c3a7e080a5c33f68bbd041b2fc0a
tttlkkkl <yehong0000@163.com>
2017-04-08 17:15:25 +0800

    错误显示级别

--- api/index.php
+++ api/index.php
@@ -6,7 +6,7 @@ $HTTP_RAW_POST_DATA = file_get_contents('php://input');
 
 $application = new Yaf\Application(APPLICATION_PATH . "/conf/application.ini");
 header("Content-Type:text/html;charset=UTF-8");
-error_reporting(E_ERROR | E_PARSE);
+error_reporting(E_ALL);
 $application->bootstrap()->run();
 //$application->getDispatcher()->dispath(new Yaf\Request_Simple());
 ?>
